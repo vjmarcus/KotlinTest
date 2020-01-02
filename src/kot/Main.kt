92987326c428d38ca56ebb18cb42126602d510d1
vjmarcus <vjmarcus1@gmail.com>
2020-01-02 19:57:41 +0300

    Nullable types

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -1,7 +1,10 @@
 package kot
 
-fun main() {
-    val line = "Oleg"
-    println(line)
 
+fun main() {
+    val a: String? = null
+    val b: String? = "234234"
+    val c: String? = ""
+    val result: Int? = (a?.length?:0) + (b?.length?:0) + (c?.length?:0)
+    println(result)
 }
