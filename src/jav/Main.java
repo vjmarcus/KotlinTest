380d9e46dcfbbf1122375ecc5cb1b815d2fc5555
vjmarcus <vjmarcus1@gmail.com>
2020-01-03 23:14:29 +0300

    When statement

--- src/jav/Main.java
+++ src/jav/Main.java
@@ -1,7 +1,23 @@
 package jav;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        String mounth;
+        System.out.println("Введите первую букву месяца на английском");
+        Scanner scanner = new Scanner(System.in);
+        mounth = scanner.nextLine();
+        switch (mounth) {
+            case "J":
+                System.out.println("January");
+                break;
+            case "F":
+                System.out.println("February");
+                break;
+            case "M":
+                System.out.println("Mart");
+                break;
+        }
     }
 }
--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -4,16 +4,32 @@ import java.util.*
 
 
 fun main() {
-    val food: String
-    val count = 400
-    food = if (count > 500) {
-        "Пицца"
-    } else if (count > 300) {
-        "Шаурма"
-    } else {
-        "Яблоко"
+    val month: String
+    println("Введите первую букву месяца на английском")
+    val scanner = Scanner(System.`in`)
+    month = scanner.nextLine()
+    when (month) {
+        "J" -> println("January")
+        "F" -> println("February")
+        "M" -> println("Mart")
+        else -> println("Не найдено")
     }
-    println(food)
+
+
+
+
+//    var count = 400
+//    val food = if (count > 500) {
+//        count -= 100
+//        "Пицца"
+//    } else if (count > 300) {
+//        count -= 200
+//        "Шаверма"
+//    } else {
+//        "Яблоко"
+//    }
+//    println(food)
+//    println("Количество денег: $count, вы едите - $food, длина строки - ${food.length}")
 
 
 //    val a: String? = null
