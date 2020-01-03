2f0e929f997204cf1f89cbd717bb2193c670342e
vjmarcus <vjmarcus1@gmail.com>
2020-01-03 22:50:16 +0300

    if statement

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -1,10 +1,24 @@
 package kot
 
+import java.util.*
+
 
 fun main() {
-    val a: String? = null
-    val b: String? = "234234"
-    val c: String? = ""
-    val result: Int? = (a?.length?:0) + (b?.length?:0) + (c?.length?:0)
-    println(result)
+    val food: String
+    val count = 400
+    food = if (count > 500) {
+        "Пицца"
+    } else if (count > 300) {
+        "Шаурма"
+    } else {
+        "Яблоко"
+    }
+    println(food)
+
+
+//    val a: String? = null
+//    val b: String? = "234234"
+//    val c: String? = ""
+//    val result: Int? = (a?.length?:0) + (b?.length?:0) + (c?.length?:0)
+//    println(result)
 }
