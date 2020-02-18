8b713eb494131ffea5d34e8877f9916d7db40ea2
vjmarcus <vjmarcus1@gmail.com>
2020-02-18 17:55:42 +0300

    bug fix

--- src/static/Main.kt
+++ src/static/Main.kt
@@ -3,9 +3,12 @@ package static
 /*companion object as static*/
 fun main() {
     println(Calc.square(10))
+    println(Calc.lengthOfCircle(10f))
 }
 class Calc{
     companion object {
+        private const val PI = 3.14
         fun square(num: Int) = num * num
+        fun lengthOfCircle(radius: Float) = 2 * PI * radius
     }
 }
